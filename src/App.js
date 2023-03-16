66d0530973583a3c54fc81985d7af66eca44a79b
janeezy <janeezyofficial@gmail.com>
2023-03-16 17:38:40 +0000

    my second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import ExpenseItems from './components/ExpenseItems';
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+			<ExpenseItems></ExpenseItems>
+		</div>
+	);
 }
 
 export default App;
