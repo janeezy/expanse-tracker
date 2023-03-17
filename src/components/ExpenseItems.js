97128ef07bd595c3fae1cec209babec52ba789b1
janeezy <janeezyofficial@gmail.com>
2023-03-17 07:56:17 +0000

    commit changes
    commit changes

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,12 +1,16 @@
 import './ExpenseItems.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+	const expenseDate = new Date(2021, 2, 28);
+	const expenseTitle = 'Car Insurance';
+	const expenseAmount = 293.67;
+
 	return (
 		<div className='expense-item'>
-			<div>March 28th 2021</div>
+			<div>{expenseDate.toISOString()}</div>
 			<div className='expense-item__description'>
-				<h2>Car Insurance</h2>
-				<div className='expense-item__price'>$296.07</div>
+				<h2>{props.title}</h2>
+				<div className='expense-item__price'>${expenseAmount}</div>
 			</div>
 		</div>
 	);
